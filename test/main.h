afca0d1cc199927ad4dec4fbf7c584eb5ef1db28
Xai Betancourt <betancourtxavier77@gmail.com>
2023-03-29 17:55:24 +0000

    removing unnecesary files

---
+++ test/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int print_string(char *c);
+
+int print_letter(char c);
+
+int _printf(const char *format, ...);
+
+int _putchar(char c);
+
+
+#endif
---
+++ test/print_letter.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_letter - print only a single letter
+ * @c: character to be given
+ * Return: 1
+ */
+
+int print_letter(char c)
+{
+	_putchar(c);
+
+	return (1);
+}
