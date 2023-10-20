72fbf097f39b691f608a0e8f9f0ac07b66914add
Xai_Betancourt~ <betancourtxavier77@gmail.com>
2023-10-20 15:55:22 -0400

    Adding comments to print_letter

--- print_letter.c
+++ print_letter.c
@@ -8,7 +8,7 @@
 
 int print_letter(char c)
 {
-	_putchar(c);
+	_putchar(c); // Call the '_putchar' function to print the character 'c'.
 
-	return (1);
+	return (1); // Return 1 to indicate that one character has been printed.
 }
