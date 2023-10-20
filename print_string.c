cfcf07175f8c0bbb741ce63eec72d66bb92c4c9b
Xai_Betancourt~ <betancourtxavier77@gmail.com>
2023-10-20 15:56:38 -0400

    Adding comments to print_string

--- print_string.c
+++ print_string.c
@@ -1,19 +1,19 @@
-#include "main.h"
+#include "main.h"  // Include the "main.h" header file for function prototypes and declarations.
 
 /**
  * print_string - print a whole string
  * @c: string that will be passed
  * Return: the length of the string
  */
-
 int print_string(char *c)
 {
-	int k = 0;
+	int k = 0;  // Initialize a variable 'k' to keep track of the character index.
 
-	while (c[k] != '\0')
+	while (c[k] != '\0')  // Loop through the characters in the string until the null terminator '\0' is reached.
 	{
-		_putchar(c[k]);
-		k++;
+		_putchar(c[k]);  // Call the '_putchar' function to print the character at index 'k'.
+		k++;  // Increment 'k' to move to the next character in the string.
 	}
-	return (k);
+
+	return (k);  // Return the value of 'k' which represents the length of the string.
 }
